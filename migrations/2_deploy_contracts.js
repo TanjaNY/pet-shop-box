d96c8fefc736ff89bf00bfeb06d7b19292bea0a2
TanjaNY <tanja.nyberg@yahoo.com>
2020-07-22 21:43:50 +0200

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+// JavaScript source codevar Adoption = artifacts.require("Adoption");
+var Adoption = artifacts.require("Adoption");
+
+module.exports = function (deployer) {
+    deployer.deploy(Adoption);
+};
