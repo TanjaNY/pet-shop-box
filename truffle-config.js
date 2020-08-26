f360f7a8e7c656a75431e9eafd6c0e9601aa5205
Tanja Nyberg <32233040+TanjaNY@users.noreply.github.com>
2020-08-26 10:54:51 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,28 @@
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = 'rabbit seek urge copy carry tank february sudden skate neutral runway know';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*"
+        },
+        develop: {
+            port: 8545
+        },
+        ropsten: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/38ff07b6625d4ba9b68615a19f54b50d`);
+            },
+            network_id: '3',
+                    },
+        test: {
+            provider: function () {
+                return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/");
+            },
+            network_id: '*',
+            gas: 4000000
+        },
     },
-    develop: {
-      port: 8545
-    }
-  }
 };
